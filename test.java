d96b91aa13a964785cb69d83bec5060437268e4b
Samruddhi-0605 <141330722+Samruddhi-0605@users.noreply.github.com>
2023-08-22 21:32:23 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,30 @@
+package testngTutorial;
+
+import org.testng.annotations.Test;
+
+public class Prgm4 {
+	@Test
+	
+	public void WebLoginHomloan()
+	{
+		
+	//selenium
+	System.out.println("WebLoginHomloan");
+	}
+	
+	@Test
+	public void MobileLoginHomeloan()
+	{
+		System.out.println("MobileLoginHomeloan");
+	}
+
+	
+	@Test
+	public void LoginAPIcarloan()
+	{
+		System.out.println("LoginAPIHomeloan");
+	}
+
+	
+	
+}
